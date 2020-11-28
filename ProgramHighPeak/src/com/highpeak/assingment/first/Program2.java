125ce59fd40106cce9371595cbb42de67304bd81
ParthAmaliya <75160343+ParthAmaliya@users.noreply.github.com>
2020-11-28 17:01:18 +0530

    Add files via upload

---
+++ ProgramHighPeak/src/com/highpeak/assingment/first/Program2.java
@@ -0,0 +1,81 @@
+package com.highpeak.assingment.first;
+
+import java.util.Scanner;
+
+public class Program2
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+	
+		int n, n1 = 0, n2 = 0, n3 = 0, n4 = 0, n5 = 0, n6 = 0;
+		String name;
+		
+		System.out.println("Number of the employees: ");
+		n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++)
+		{
+			name = sc.next();
+	
+			switch (n) 
+			{
+			case 1:
+				n1 = sc.nextInt();
+				System.out.println(name +" : "+ n1);
+				break;
+			
+			case 2:
+				n2 = sc.nextInt();
+				System.out.println(name +" : "+ n2);
+				break;
+				
+			case 3:
+				n3 = sc.nextInt();
+				System.out.println(name +" : "+ n3);
+				break;
+				
+			case 4:
+				n4 = sc.nextInt();
+				System.out.println(name +" : "+ n4);
+				break;
+				
+			case 5:
+				n5 = sc.nextInt();
+				System.out.println(name +" : "+ n5);
+				break;
+				
+			case 6:
+				n4 = sc.nextInt();
+				System.out.println(name +" : "+ n6);
+				break;
+			}
+		}
+		int[] a = {n1,n2,n3,n4,n5,n6};
+		test(a);
+		
+		for (int j = 0; j < a.length; j++)
+		{
+			System.out.println(a[j]);
+		}
+		System.out.println(a[0] - a[a.length-1]);
+		
+		sc.close();
+	}
+	public static void test(int[] a)
+	{
+		int n = a.length;
+		for (int i = 0; i < n-1; i++)
+		{
+			for (int j = 0; j < n-i-1; j++)
+			{
+				if (a[j] > a[j+1])
+				{
+					int temp = a[j];
+					a[j] = a[j+1];
+					a[j+1] = temp;
+				}
+			}
+		}
+	}
+}
---
+++ ProgramHighPeak/src/com/highpeak/assingment/first/Program3.java
@@ -0,0 +1,70 @@
+package com.highpeak.assingment.first;
+
+import java.util.Scanner;
+
+public class Program3
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+	
+		int n, n1 = 0, n2 = 0, n3 = 0, n4 = 0;
+		String name;
+		
+		System.out.println("Number of the employees: ");
+		n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++)
+		{
+			name = sc.next();
+	
+			switch (n) 
+			{
+			case 1:
+				n1 = sc.nextInt();
+				System.out.println(name +" : "+ n1);
+				break;
+			
+			case 2:
+				n2 = sc.nextInt();
+				System.out.println(name +" : "+ n2);
+				break;
+				
+			case 3:
+				n3 = sc.nextInt();
+				System.out.println(name +" : "+ n3);
+				break;
+				
+			case 4:
+				n4 = sc.nextInt();
+				System.out.println(name +" : "+ n4);
+				break;
+			}
+		}
+		int[] a = {n1,n2,n3,n4};
+		test(a);
+		
+		for (int j = 0; j < a.length; j++)
+		{
+			System.out.println(a[j]);
+		}
+		System.out.println(a[0] - a[a.length-1]);
+		sc.close();
+	}
+	public static void test(int[] a)
+	{
+		int n = a.length;
+		for (int i = 0; i < n-1; i++)
+		{
+			for (int j = 0; j < n-i-1; j++)
+			{
+				if (a[j] > a[j+1])
+				{
+					int temp = a[j];
+					a[j] = a[j+1];
+					a[j+1] = temp;
+				}
+			}
+		}
+	}
+}
